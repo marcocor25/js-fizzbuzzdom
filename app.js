54a5cae569ac7adc81d072301b59a8f78ac326d8
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-07 21:44:58 +0100

    Collego elementi JS all'HTML

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-// TEST
-console.warn('Mind blowing...');
+// COLLEGO GLI ELEMENTI JS ALL'HTML
+
+let container = document.getElementById('main_content');
+
+// HO CREATO UN CICLO DI NUMERI DA 1 A 100
+
+for (let i = 1; i < 101; i++) {
+    console.log(i);
+
+    const square = document.createElement('div');
+    container.append(square);
+    square.classList.add('square-content');
+
+}
